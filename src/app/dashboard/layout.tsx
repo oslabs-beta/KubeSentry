9c88a87bbc7078b21826518e12c803524944b0f8
bisectors <gun.clawvuthinan@gmail.com>
2023-12-05 11:29:15 -0500

    Add banner

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import Sidebar from '@/src/app/ui/sidebar';
+import Banner from '../components/Banner';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden bg-primaryDark text-slate-400'>
-      <div className='w-full flex-none md:w-64'>
-        <Sidebar />
+    <>
+      <Banner />
+      <div className='flex h-screen flex-col md:flex-row md:overflow-hidden bg-primaryDark text-slate-400'>
+        <div className='w-full flex-none md:w-64'>
+          <Sidebar />
+        </div>
+        <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
       </div>
-      <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
-    </div>
+    </>
+    
   );
 }
