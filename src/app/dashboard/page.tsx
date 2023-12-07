04e39a99d9f12e049a00f682b8cecfd80a63b7c4
MXW9699 <mitchell.wen@hotmail.com>
2023-12-07 17:05:19 -0500

    increased refresh rate of page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,10 +2,9 @@
 import React, { useState, ChangeEvent } from 'react';
 import SearchBar from '../components/Searchbar';
 
-import LinePlot from '../charts/LinePlot'
-import PieChart from '../charts/PieChart'
-import TimeSeriesPlot from '../charts/TimeSeriesPlot'
-
+import LinePlot from '../charts/LinePlot';
+import PieChart from '../charts/PieChart';
+import TimeSeriesPlot from '../charts/TimeSeriesPlot';
 
 export default function Page() {
   //set state and function to handle change in search input
@@ -21,26 +20,28 @@ export default function Page() {
     setSearchQuery('');
   }
 
-
-  const style = { width: "50%" };
-
+  const style = { width: '50%' };
 
   return (
-    <div className='bg-secondaryDark rounded-md text-white min-h-screen p-8'>
+    <div className="bg-secondaryDark rounded-md text-white min-h-screen p-8">
       <SearchBar
         value={searchQuery}
         onChange={handleSearch}
         onSearch={handleSearchButtonClick}
       />
       <h1 className="text-3xl font-bold mt-4">Sentry Dashboard</h1>
-      <div style={style}>
-        <TimeSeriesPlot />
+      <div className="flex flex-column">
+        <div style={style}>
+          <TimeSeriesPlot />
+        </div>
+        <div style={style}>
+          <LinePlot />
+        </div>
       </div>
-      <div style={style}>
-        <LinePlot />
-      </div>
-      <div style={style}>
-        <PieChart />
+      <div className="flex flow-row justify-center">
+        <div style={style}>
+          <PieChart />
+        </div>
       </div>
     </div>
   );
