bd123cdb35127f63d2d6584c09f752e2f4a31be4
MXW9699 <mitchell.wen@hotmail.com>
2023-12-08 11:32:32 -0500

    refactored code and added comments to the delete pods functionality. made enclosed for loop for making podcard arrays in a function.

--- src/app/dashboard/pods/page.jsx
+++ src/app/dashboard/pods/page.jsx
@@ -3,57 +3,65 @@ import PodCard from '@/src/app/ui/PodCard';
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-const fetchData = async () => {
-  const response = await fetch('http://localhost:3000/podapi');
-  const data = await response.json();
-  // console.log(data['pods'][0]);
-  return data['pods'];
-};
 export default function Page() {
-  const [pods, setPods] = useState([]);
-  let podsArray = [];
-  const fetchPod = async () => {
-    const responseData = await fetchData();
-    setPods(responseData);
+  /*******************************USE STATE**************************************** */
+  const [pods, setPods] = useState([]); //array of pod objects.
+  /*******************************HELPER FUNCTION**************************************** */
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:3000/podapi');
+    const data = await response.json();
+    setPods(data['pods']);
   };
-
+  //handle click function to be passed to the pod cards to delete
   const handleClick = async (name, namespace) => {
-    console.log(name, namespace);
-    const data = await fetch('http://localhost:3000/podapi', {
+    //send a delete request to the next.js endpoint
+    await fetch('http://localhost:3000/podapi', {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name: name, namespace: namespace }),
     });
-    const result = await data.json();
-    console.log(result);
+    return;
+  };
+  //make pods cards from the data we get back
+  const makePodCards = () => {
+    //initialize empty array
+    let podsArray = [];
+    //for each pod, push a card passing in props for name, namespace, status and the handleclick function
+    for (let i = 0; i < pods.length; i++) {
+      podsArray.push(
+        <div>
+          <PodCard
+            podName={pods[i]['name']}
+            podStatus={pods[i]['status']}
+            nameSpace={pods[i]['namespace']}
+            handleClick={handleClick}
+          />
+        </div>
+      );
+    }
+    //return the populated array
+    return podsArray;
   };
 
+  /*******************************USE EFFECT**************************************** */
+  //only once on load
   useEffect(() => {
-    fetchPod();
-
-    const intervalId = setInterval(fetchPod, 3000);
-
+    //setup code
+    //get pod data
+    fetchData();
+    //keep fetching every 3 seconds
+    const intervalId = setInterval(fetchData, 3000);
+    //cleanup code
     return () => {
+      //clear the existing set interval
       clearInterval(intervalId);
     };
   }, []);
 
-  for (let i = 0; i < pods.length; i++) {
-    podsArray.push(
-      <div>
-        <PodCard
-          podName={pods[i]['name']}
-          podStatus={pods[i]['status']}
-          nameSpace={pods[i]['namespace']}
-          handleClick={handleClick}
-        />
-      </div>
-    );
-  }
   return (
     <div>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {podsArray}
+        {makePodCards()}
       </div>
     </div>
   );
--- src/app/podapi/route.ts
+++ src/app/podapi/route.ts
@@ -1,23 +1,39 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  let response = await fetch('http://localhost:8888/metrics/kubepods', {
-    cache: 'no-store',
-  });
-  let data = await response.json();
-  return NextResponse.json(data);
+  try {
+    let response = await fetch('http://localhost:8888/metrics/kubepods', {
+      cache: 'no-store',
+    });
+    let data = await response.json();
+    return NextResponse.json(data);
+  } catch (err) {
+    //return error message back to NEXT.JS endpoint
+    return NextResponse.json({
+      log: 'could not get pods in next',
+      status: 500,
+      message: { err: err },
+    });
+  }
 }
 
 //delete a pod
 export async function DELETE(request: Request) {
-  const { name, namespace } = await request.json();
-  console.log('in next server', name, namespace);
-  let response = await fetch(
-    `http://localhost:8888/metrics/delete/${namespace}/${name}`,
-    {
+  try {
+    //extract name, namespace from request coming in as an object
+    const { name, namespace } = await request.json();
+    //fetch to the express server endpoint
+    await fetch(`http://localhost:8888/metrics/delete/${namespace}/${name}`, {
       cache: 'no-store',
-    }
-  );
-  let data = await response.json();
-  return NextResponse.json(`pod ${name} iin ${namespace} was deleted`);
+    });
+    //return response back to the NEXT.JS endpoint
+    return NextResponse.json(`pod ${name} in ${namespace} was deleted`);
+  } catch (err) {
+    //return error message back to NEXT.JS endpoint
+    return NextResponse.json({
+      log: 'could not delete pod in next',
+      status: 500,
+      message: { err: err },
+    });
+  }
 }
