019e4cd58da3d83f8845f2ec4ca3330cfd39dacb
Kevin Fong <kevinkimfong@gmail.com>
2023-12-14 13:12:44 -0500

    restructured divs to integerate better styling

--- src/app/dashboard/pods/page.jsx
+++ src/app/dashboard/pods/page.jsx
@@ -60,7 +60,7 @@ export default function Page() {
 
   return (
     <div>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
         {makePodCards()}
       </div>
     </div>
--- src/app/ui/PodCard.tsx
+++ src/app/ui/PodCard.tsx
@@ -9,18 +9,20 @@ type cardProps = {
 export default function PodCard(props: cardProps) {
   const { podName, podStatus, nameSpace, handleClick } = props;
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg border-solid bg-secondaryDark shadow-lg ">
-      <div className="flex flex-col items-center px-4 py-4 h-64">
-        <div className="font-bold text-base mb-2 text-center text-slate-100">
+    <div className='flex flex-col justify-between h-64 rounded-3xl overflow-hidden shadow-lg border-solid bg-secondaryDark shadow-lg '>
+      <div className='m-4'>
+        <div className='font-bold text-base mb-2 text-center text-slate-100'>
           {podName}
         </div>
-        <p className="text-gray-700 text-sm text-center">{nameSpace}</p>
-        <p className="text-gray-700 text-sm text-center text-green-600">
+        <p className='text-gray-700 text-sm text-center'>{nameSpace}</p>
+        <p className='text-gray-700 text-sm text-center text-green-600'>
           {podStatus}
         </p>
+      </div>
+      <div className='flex justify-center w-full pb-4'>
         <button
           onClick={() => handleClick(podName, nameSpace)}
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-12"
+          className='bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded'
         >
           Delete
         </button>
