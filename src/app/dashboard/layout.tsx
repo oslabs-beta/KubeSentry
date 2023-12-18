87dc7e71faddf486a8354ec97e05cf2e1f3c5b7e
MXW9699 <mitchell.wen@hotmail.com>
2023-12-18 11:45:38 -0500

    fixed 500 server error caused by looking for localstorage banner conditional before the page is loaded. Banner is now conditional rendered checking the local storage after the intial load. It will render if the showbanner key is non-existant in local storage and is initially set to not show

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,19 +1,25 @@
-"use client";
-import React, { useState } from "react";
-import Sidebar from "@/src/app/ui/sidebar";
-import Banner from "../components/Banner";
+'use client';
+import React, { useState, useEffect } from 'react';
+import Sidebar from '../../../src/app/ui/sidebar';
+import Banner from '../components/Banner';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  let initialBanner = true;
-  //the first time user come to the page localstorage is gonna be empty, we are making sure that user can always see the banner the first time he visit and then after he close it and refresh he will not see it again
-  if (localStorage.getItem("userCloseBanner")) {
-    initialBanner = false;
-  }
-  const [showbanner, setShowBanner] = useState(initialBanner);
+  //the first time user come to the page localstorage is gonna be empty,
+  //we are making sure that user can always see the banner the first time he visit and then after he close it and refresh he will not see it again
+  const [showbanner, setShowBanner] = useState(false);
+
   function closeBanner() {
     setShowBanner(false);
-    localStorage.setItem("userCloseBanner", "true");
+    localStorage.setItem('showBanner', 'false');
+    console.log(localStorage);
   }
+
+  useEffect(() => {
+    //if showbanner doesnt exist, set show banner to true
+    //if showbanner does exist, and is true, set showbanner to false
+    return setShowBanner(!localStorage.getItem('showBanner'));
+  }, []);
+
   return (
     <>
       {showbanner && <Banner closeBanner={closeBanner} />}
