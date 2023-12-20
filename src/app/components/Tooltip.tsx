28cb13a9e0a08031d505895407288ed5178d9e5e
Michael Abrahams <miabraha@gmail.com>
2023-12-20 12:54:24 -0500

    Add tooltip on hover

--- src/app/components/Tooltip.tsx
+++ src/app/components/Tooltip.tsx
@@ -3,15 +3,44 @@ import React, { useState } from 'react';
 interface TooltipProps { children: React.ReactNode[], text: string};
 
 export const Tooltip = (props: TooltipProps) => {
-  const [isHovering, setIsHovering] = useState(false);
+  const [hoverTimeout, setHoverTimeout] = useState<ReturnType<typeof setTimeout>|null>(null);
+  const [isShown, setIsShown] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    setMousePosition({
+      x: e.nativeEvent.offsetX,
+      y: e.nativeEvent.offsetY
+    });
+  };
 
   return (
     <div
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
+      onMouseEnter={() => {
+        console.log(props.text);
+        setHoverTimeout(setTimeout( () => { setIsShown(true) }, 300));
+      }}
+      onMouseLeave={() => {
+        if (hoverTimeout) clearTimeout(hoverTimeout);
+        setHoverTimeout(null);
+        setIsShown(false)
+    }}
+      onMouseMove={handleMouseMove}
     >
       {props.children}
-      {isHovering && <div className="tooltip">{props.text}</div>}
+      {isShown && (
+        <div
+          className="tooltip"
+          style={{
+            left: mousePosition.x,
+            top: mousePosition.y
+          }}
+        >
+          {props.text}
+        </div>
+      )}
     </div>
   );
 };
+
+export default Tooltip;
